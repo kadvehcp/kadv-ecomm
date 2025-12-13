81ad7b72c4b4ccfa4ff5fa57d5a85158add9dbbd
kadvehcp <kadvehcp@gmail.com>
2025-12-13 21:58:00 +0530

    added tailwindcss support and formatted all files

---
+++ kadv-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-4xl text-teal-500">Hello, World!</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;

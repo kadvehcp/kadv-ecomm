d512eb888e0b761ca9220778d29c5cd256cdc71c
kadvehcp <kadvehcp@gmail.com>
2026-03-21 22:06:53 +0000

    installed vite project touched every file a little formatted with prettier

---
+++ kadv-frontend/readme.md
@@ -0,0 +1,24 @@
+# kadv-ecomm
+
+A basic **E-commerce project** built using the **MERN Stack**.
+
+---
+
+## Frontend
+
+A simple frontend built using **Vite** and **React**.
+
+---
+
+## Technologies Used
+
+- **Vite** – Fast frontend build tool
+- **React (with React Compiler)** – UI library
+- **Tailwind CSS** - CSS framework
+
+---
+
+## Author
+
+**Kamal Kishor**  
+GitHub: [@kadvehcp](https://github.com/kadvehcp)
--- kadv-frontend/vite.config.js
+++ kadv-frontend/vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: { host: true },
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
-})
+});
